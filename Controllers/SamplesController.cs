eff6422a5c412f263c0c926184c737ef75baa491
IanWinpenny <ianwinpenny@gmail.com>
2016-03-17 23:31:25 +1100

    Test

---
+++ Controllers/SamplesController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class SamplesController : ApiController
+    {
+        private ATM_DBEntities db = new ATM_DBEntities();
+
+        // GET: api/Samples
+        public IQueryable<Sample> GetSamples()
+        {
+            return db.Samples;
+        }
+
+        // GET: api/Samples/5
+        [ResponseType(typeof(Sample))]
+        public IHttpActionResult GetSample(decimal id)
+        {
+            Sample sample = db.Samples.Find(id);
+            if (sample == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(sample);
+        }
+
+        // PUT: api/Samples/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutSample(decimal id, Sample sample)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != sample.SampleNo)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(sample).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SampleExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Samples
+        [ResponseType(typeof(Sample))]
+        public IHttpActionResult PostSample(Sample sample)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Samples.Add(sample);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (SampleExists(sample.SampleNo))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = sample.SampleNo }, sample);
+        }
+
+        // DELETE: api/Samples/5
+        [ResponseType(typeof(Sample))]
+        public IHttpActionResult DeleteSample(decimal id)
+        {
+            Sample sample = db.Samples.Find(id);
+            if (sample == null)
+            {
+                return NotFound();
+            }
+
+            db.Samples.Remove(sample);
+            db.SaveChanges();
+
+            return Ok(sample);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool SampleExists(decimal id)
+        {
+            return db.Samples.Count(e => e.SampleNo == id) > 0;
+        }
+    }
+}
